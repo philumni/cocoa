144bd33403f40eef2d0a9b5cc00b1c2a06c43fd6
philumni <129319660+philumni@users.noreply.github.com>
2023-04-28 15:07:49 -0700

    Update server.js

--- server.js
+++ server.js
@@ -3,5 +3,6 @@ const host = 'localhost';
 const port = 3000;
 const requestListener = function (req, res) {};
 const server = http.createServer(requestListener);
+console.log("server starting.");
 server.listen(port, host, () => { console.log(`Server is running on http://${host}:${port}`); }); 
 
